46c1a39041d89325d849339ebab0854a91f520d4
Pyre Flos <pyreflos@gmail.com>
2015-12-03 12:18:58 -0800

    Create rtl-config.py

---
+++ rtl-config.py
@@ -0,0 +1,6 @@
+##
+##  User configuration file - edit these settings to suit your own project
+##
+file_path = '/home/pi/RPi-RTL/images/'  ##  path to save images
+file_prefix = 'img_'  ##  prefix before timestamp.jpg, if needed - e.g. a project number
+use_timestamp = True  ##  True = timestamp in filename, False = incremental numbering
