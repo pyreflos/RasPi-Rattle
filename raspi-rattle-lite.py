7c6ccb846a606c6cf444817918a0c2fef7f917dd
Pyre Flos <pyreflos@gmail.com>
2015-12-03 17:26:48 -0800

    Create raspi-rattle-lite.py
    description and place-holder only

---
+++ raspi-rattle-lite.py
@@ -0,0 +1,19 @@
+##  
+##  RASpberry PI - RemoTe Time LapsE - Lite Edition
+##  
+##  ===== =====   Version 0.0   ===== =====
+##  
+##  The lite version is the same as the full version,
+##  but is intended to run headless, without wifi,
+##  and possible on a battery. It has no unload
+##  function, but does require an RTC circuit.
+##  
+##  Required hardware:
+##      Raspberry Pi, Raspberry Pi Camera, DS1307 RTC, 5V Power Source
+##
+##  Dependencies:
+##      Dropbox-Uploader by andreafabrizi (https://github.com/andreafabrizi/Dropbox-Uploader)
+##  
+
+import time
+import picamera
